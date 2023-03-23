cbbd757dce7613222d9a3eaab741969dfb217f91
Jani Pirisjoki <jani.pirisjoki@helsinki.fi>
2023-03-23 09:20:26 +0200

    Add task for starting the app

---
+++ harjoitustyo/tasks.py
@@ -0,0 +1,12 @@
+from invoke import task
+
+import shutil
+
+if shutil.which("python") is not None:
+    python_executable = "python"
+else:   
+    python_executable = "python3"
+    
+@task
+def start(ctx):
+    ctx.run(f"{python_executable} run.py", pty=False)
