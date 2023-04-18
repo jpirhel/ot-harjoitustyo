6c5b7b78fbb74cd4523db0daeef9068e53bd4b14
Jani Pirisjoki <jani.pirisjoki@helsinki.fi>
2023-04-18 20:19:15 +0300

    Run: Add argument parser for running import-data-from-web

--- harjoitustyo/run.py
+++ harjoitustyo/run.py
@@ -1,9 +1,23 @@
+import argparse
+
 from harjoitustyo.app import App
+from harjoitustyo.engine.data.importer import Importer
 
 
 def main():
-    app = App()
-    app.run()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--import-data-from-web", action="store_true")
+    args = parser.parse_args()
+
+    import_data_from_web = args.import_data_from_web
+
+    if import_data_from_web:
+        importer = Importer()
+        importer.import_data()
+    else:
+        app = App()
+        app.run()
 
 
 if __name__ == "__main__":
